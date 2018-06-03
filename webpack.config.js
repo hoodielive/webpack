1fd1ef9c0d36aaf56b7be89c48ec5df19e209401
hoodielive <hoodielive@gmail.com>
2018-06-03 16:31:38 -0400

    using sass firehose in my build lol

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 
             {
                 test: /\.s[ac]ss$/,
-                use: ['sass-loader'] 
+                use: ['style-loader', 'css-loader', 'sass-loader'] 
            
             },
 
